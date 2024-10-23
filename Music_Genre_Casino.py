05d33f8066dcc478f0a68be590adae1768bbf9cb
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2024-10-22 19:06:24 -0500

    finished some of the genre list

---
+++ Music_Genre_Casino.py
@@ -0,0 +1,24 @@
+import random
+Genre_List = ["Crossover Music","Danger Music", "Drone Music", "Electroacoustic", "Industrial Music","Instrumental Music", "Lofi Music",
+                    "Musical Improvisation", "Musique concrete", "Noise", "Outsider Music", "Progessive Music", "Psychedelic Music", "Underground music",
+                    "African Blues", "Blues Rock", "British Bules", "Canadian Blues", "Chicago Blues", "Classic Female Blues", "Contempoary R&B",
+                    "Country Blues", "Delta Blues", "Desert Blues", "Detroit Blues", "Electric Blues", "Gospel Blues", "Hill Country Blues",
+                    "Hokum Blues", "Jump Blues", "Kansas City Blues", "Louisiana Blues", "Memphis Blues", "New Orleans Blues", "Piedmont Blues", 
+                    "Punk Blues", "Rhythm and Blues", "Soul Blues", "St. Louis Blues", "Swamp Blues", "Talking Blues", "Texas Blues", "West Coast Blues",
+                    "Alternative Country", "Australian Country", "Bakersfield Sound", "Bluegrass", "Blue Yodeling", "Bro-Country", "Cajun",
+                    "Canadian Country", "Christian Country", "Classic Country", "Country and Irish", "Country Blues", "Country en Espanol",
+                    "Country Folk", "Country Pop", "Country Rap", "Country Rock", "Cowboy Pop", "Dansband", "Gulf and Western", "Hokum", "Honky Tonk",
+                    "Instrumental Country", "Lubbock Sound", "Nashville Sound", "Neotraditional Country", "Old-time", "Outlaw Country", "Pop Country",
+                    "Progressive Country", "Regional Mexican", "Rockabilly", "Southern Rock", "Southern Soul", "Sertanejo", "Talking Blues",
+                    "Traditional Country", "Truck-Driving Country", "Western Music", "Zydeco", "Adult Contemporary Music", "Adult Standards",
+                    "Background Music", "Barococo", "Beautiful Music", "Chill-Out", "Downtempo", "Furniture Music", "Light Music", "Lounge Music",
+                    "Middle of the Road", "New-Age Music", "Soft Rock",]
+
+
+def Main():
+    print("Penis Fart")
+
+
+ 
+if __name__ == "__main__":
+    Main()
