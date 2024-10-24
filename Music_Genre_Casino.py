aacc7c07b3589a5960a2df71fd7e8baecf5e5bf1
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2024-10-23 21:57:18 -0500

    More genres added

--- Music_Genre_Casino.py
+++ Music_Genre_Casino.py
@@ -12,11 +12,40 @@ Genre_List = ["Crossover Music","Danger Music", "Drone Music", "Electroacoustic"
                     "Progressive Country", "Regional Mexican", "Rockabilly", "Southern Rock", "Southern Soul", "Sertanejo", "Talking Blues",
                     "Traditional Country", "Truck-Driving Country", "Western Music", "Zydeco", "Adult Contemporary Music", "Adult Standards",
                     "Background Music", "Barococo", "Beautiful Music", "Chill-Out", "Downtempo", "Furniture Music", "Light Music", "Lounge Music",
-                    "Middle of the Road", "New-Age Music", "Soft Rock",]
+                    "Middle of the Road", "New-Age Music", "Soft Rock", "Ambient dub", "Dark ambient" , "Ambient industrial",  "Dungeon synth",
+                    "Isolationism", "Dreampunk", "Illbient", "Space music", "Footwork", "Future bass", "Kawaii future bass", "Jungle terror",
+                    "Midtempo bass", "Trap", "UK bass", "Wave", "Hardwave", "Acid breaks", "Baltimore club", "Jersey club", "Philly club", "Big beat",
+                    "Breakbeat hardcore", "Darkcore", "Hardcore breaks", "Broken beat", "Florida breaks", "Nu Skool Breaks", "Progressive Breaks",
+                    "Psychedelic Breakbeat", "Downtempo", "Psybient", "Trip Hop", "Trip Rock", "Afro Music", "Electro-Disco", "Eurobeat",
+                    "Italo Disco", "Space Disco", "Eurodisco", "Nu-Disco", "Boogie", "City Pop", "Darkstep", "Drumfunk", "Drumstep", "Hardstep",
+                    "Jazzstep", "Liquid Funk", "Neurofunk", "Sambass", "Techstep", "Dub Poetry", "Dubtronica", "Dance-Rock", "Dance-Punk", "Dance-Pop",
+                    "Hyperpop", "Sophisti-Pop", "Synth-Pop", "Wonky Pop", "Indietronica", "Krautrock", "New Wave", "Ethereal Wave", "Minimal Wave",
+                    "New Romantic", "Post-Rock", "Space Rock", "Synth-Metal", "Electrogrind", "Synth-Punk", "Electronica", "Folktronica", "Nu Jazz", 
+                    "Progressive Electronic", "Jungle", "Hardcore", "Bouncy Techno", "Breakcore", "Raggacore", "Digital Hardcore", "Frenchcore",
+                    "Gabber", "Happy Hardcore", "UK Hardcore", "Industrial Hardcore", "J-Core", "Speedcore", "Vaporwave", "Electro", "Emo Rap", 
+                    "Afroswing", "Instrumental Hip Hop", "Lofi Hip Hop", "Miami Bass", "Mumble Rap", "Trap", "Acid House", "Afro House", "Ambient House",
+                    "Balearic Beat", "Ballroom", "Bass House", "Brazilian Bass", "Blog House", "Chicago Hard House", "Chicago House", "Deep House", 
+                    "Disco House", "Diva House", "Electro-Industrial", "Aggrotech", "Electronic Body Music", "Futurepop", "New Beat", "Industrial Hip Hop", 
+                    "Cyber Metal", "Industrial Rock", "Martial Industrial", "Witch House", "Drill n Bass", "R&B", "Alternative R&B", "Contemporary R&B",
+                    "Neo Soul", "New Jack Swing", "Techno", "Acid Techno", "Ambient Techno", "Birmingham Sound", "Bleep Techno", "Detroit Techno",
+                    "Dub Techno", "Hard Techno", "Industrial Techno", "Minimal Techno", "Schaffel", "Toytown Techno", "Acid Trance", "Balearic Trance",
+                    "Dream Trance", "Eurotrance", "Goa Trance", "Hard Trance", "Progressive Trance", "Psychedelic Trance", "Tech Trance", "Video Game Music",
+                    "American Folk Revival", "Americana", "Anti-Folk", "British Folk Revival", "Cajun Music", "Celtic Music", "Chalga", "Corrido",
+                    "Creole Music", "Filk", "Folk Noir", "Folk Rock", "Folktronica", "Freak Folk", "Indie Folk", "Industrial Folk", "Mariachi", "Ranchera",
+                    "Neofolk", "New Weird America", "Progessive Folk", "Protest Song", "Psychedelic Folk", "Singer-Songwriter", "Skiffle", "Sung Poetry",
+                    "Alternative Hip Hop", "Boom Bap", "Bounce", "British Hip Hop", "Chopped and Screwed", "Christian Hip Hop", "Cloud Rap", "Comedy Hip Hop",
+                    "Crunk", "Crunkcore", "East Coast Hip Hop", "Freestyle Rap", "Funk Carioca", "Frat Rap", "G-Funk", "Hardcore Hip Hop", "Gangsta Rap",
+                    "Horrorcore", "Memphis Rap", "Hyphy", "Instrumental Hip Hop", "Latin Hip Hop", "Chicano Rap", "Lofi Hip Hop", "Miami Bass", "Mumble Rap",
+                    "Nerdcore", "Political Hip Hop", "Progressive Rap", "Jewish Hip Hop", "Snap Music", "Southern Hip Hop", "Trap Music", "Drill Music",
+                    "Latin Trap", "Phonk", "Plugg", "Rage", "Underground Hip Hop", "West Coast Hip Hop", "Country Rap", "Emo Rap", "Hip Hop Soul", 
+                    "Industrial Hip Hop", "Jazz Rap", "New Jack Swing", "Pop Rap", "Punk Rap", "Ragga Hip Hop", "Rap Opera", "Rap Rock", "Rap Metal", 
+                    "Rapcore", "Trip Hop",
+                    ]
 
 
 def Main():
-    print("Penis Fart")
+    Genre_Choice = random.choice(Genre_List)
+    print(Genre_Choice)
 
 
  
