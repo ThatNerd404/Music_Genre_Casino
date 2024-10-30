66f4ba237d8f3152c2a0a899945f359ba0c130e2
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2024-10-29 21:52:59 -0500

    qt designer is qt designing?

--- Music_Genre_Casino.py
+++ Music_Genre_Casino.py
@@ -1,12 +1,15 @@
 import random 
 import PySide6 
 from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtUiTools import QUiLoader
 import sys
 
 def Main():
+    loader = QUiLoader()
     app = QApplication(sys.argv)
-    window = QWidget()
+    window = loader.load("MainWindow.ui", None)
     window.show()
+    window.setWindowTitle("Music Genre Casino")
     app.exec()
 
 if __name__ == "__main__":
