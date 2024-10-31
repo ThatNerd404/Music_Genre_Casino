a20c5bd4b39cadb515e3ec4c695f6793cc0c21a9
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2024-10-30 19:51:30 -0500

    more comments

--- Main.py
+++ Main.py
@@ -11,7 +11,9 @@ if __name__ == "__main__":
     Main()
 
 #* command to turn ui files into py files: pyside6-uic input.ui > output.py 
+#* command to turn py files into exe files: pyinstaller --onefile -w pytoexe.py
 #! remember to change encoding of the  ui-to-py files to utf-8 EVERY SINGLE TIME
 #! REMEMBER YOU HAVE TO DELETE THE OLD PY FILE TO MAKE THE NEW ONE
+#! Remember to make executable for yourself make sure to have absolute path or just keep it where it is and add idk a shortcut 
 
 
